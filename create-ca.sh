d5d14acb9ff033230d2a04d99637658cf7afb50e
wlanboy <wlanboy@github.com>
2025-11-26 11:14:35 +0100

    add comments to clarify key generation options in create-ca.sh

--- create-ca.sh
+++ create-ca.sh
@@ -1,4 +1,7 @@
 cd /local-ca
+
+#prime256v1 (NIST P‑256) oder secp384r1 (NIST P‑384)
+#openssl ecparam -name prime256v1 -genkey -noout -out ca.key
 openssl ecparam -name secp384r1 -genkey -noout -out ca.key
 
 openssl req -x509 -new -nodes -key ca.key -sha256 -days 3650 \
